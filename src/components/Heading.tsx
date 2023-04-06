c5a70c39a8de37b7b992ff36868e2d4090de76a1
Sebastien Corneau <corneausebastien09@gmail.com>
2023-04-06 16:36:06 -0400

    refactor: made text a bit smaller on md breakpoint

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,14 +1,18 @@
-import {ReactElement} from 'react'
+import { ReactElement } from "react";
 
 type Props = {
-    title: string;
-    className?: string;
-}
+  title: string;
+  className?: string;
+};
 
-const Heading = ({title, className}: Props): ReactElement => {
+const Heading = ({ title, className }: Props): ReactElement => {
   return (
-    <h1 className={`text-2xl text-center lg:text-left lg:text-4xl font-bold mb-4 ${className}`}>{title}</h1>
-  )
-}
+    <h1
+      className={`mb-4 text-center text-2xl font-bold md:text-3xl lg:text-left lg:text-4xl ${className}`}
+    >
+      {title}
+    </h1>
+  );
+};
 
 export default Heading;
