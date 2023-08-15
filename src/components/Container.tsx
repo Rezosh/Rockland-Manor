d8bf779d700127ab0c2c0c00db6eb3895c97a3a3
Sebastien Corneau <corneausebastien09@gmail.com>
2023-08-15 07:31:01 -0400

    refactor: formatted quotations

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 type Children = {
-    children: React.ReactNode
-}
-const Container = ({children}: Children) => {
+  children: React.ReactNode;
+};
+const Container = ({ children }: Children) => {
   return (
-    <div className='container mx-auto max-w-7xl lg:px-8 px-4'>{children}</div>
-  )
-}
+    <div className="container mx-auto max-w-7xl px-4 lg:px-8">{children}</div>
+  );
+};
 
-export default Container
+export default Container;
