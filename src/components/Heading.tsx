5908201bcc19c8da2421e2f9c89cbf7bb119f1fe
Sebastien Corneau <corneausebastien09@gmail.com>
2023-08-15 07:30:43 -0400

    refactor: made headings smaller

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Heading = ({ title, className }: Props): ReactElement => {
   return (
     <h1
-      className={`mb-4 text-center text-2xl font-bold md:text-3xl lg:text-left lg:text-4xl ${className}`}
+      className={`mb-4 text-center text-2xl font-bold md:text-4xl lg:text-left ${className}`}
     >
       {title}
     </h1>
