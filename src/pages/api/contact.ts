2b6fa2bbb228ffd06b092fc0189c96dd7d0ce204
Sebastien Corneau <corneausebastien09@gmail.com>
2023-08-16 09:47:45 -0400

    fixed email

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,23 +1,26 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import nodemailer from "nodemailer";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { firstName, lastName, phoneNumber, email, message } = req.body;
-  let nodemailer = require("nodemailer");
 
   const transporter = nodemailer.createTransport({
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-      user: "manoirrocklandmanor@gmail.com",
-      pass: "tpcugxofoxwufovi",
+      user: process.env.CONTACT_EMAIL,
+      pass: process.env.CONTACT_EMAIL_PASSWORD,
     },
     secure: true,
   });
 
   const mailData = {
-    from: "manoirrocklandmanor@gmail.com",
-    to: "corneausebastien09@gmail.com",
+    from: process.env.CONTACT_EMAIL,
+    to: "reception@manoirrocklandmanor.com",
     subject: `New Message From Website - ${firstName} ${lastName}`,
     text: message,
     html: `<div>${message}</div><p>Sent By: ${
@@ -25,9 +28,11 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     }<br/>Email: ${email}<br/>Phone: ${phoneNumber}</p>`,
   };
 
-  transporter.sendMail(mailData, function (err: any, info: any) {
-    if (err) console.log(err);
-    else console.log(info);
-  });
+  const sentMail = await transporter.sendMail(mailData);
+
+  if (!sentMail) {
+    return res.status(500).json({ status: "ERROR" });
+  }
+
   return res.status(200).json({ status: "OK" });
 }
