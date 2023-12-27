ce1473d1c5ba698227948e3792f3860bbc5e5a63
Sebastien Corneau <corneausebastien09@gmail.com>
2023-12-27 12:07:24 -0500

    feat: added language switcher to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,7 @@
+import { cn } from "@/lib/utils";
 import useTranslation from "next-translate/useTranslation";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Footer() {
   const { t } = useTranslation("footer");
@@ -10,11 +13,13 @@ export default function Footer() {
       { name: t("jobs-link"), href: "/careers" },
     ],
   };
+  const { locale, locales } = useRouter();
+
   return (
     <footer className="bg-white">
-      <div className="mx-auto max-w-7xl overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
+      <div className="mx-auto flex max-w-xl flex-col justify-center overflow-hidden py-20 px-6 sm:py-24 lg:px-8">
         <nav
-          className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
+          className="-mb-6 flex columns-2 justify-center space-x-12"
           aria-label="Footer"
         >
           {navigation.main.map((item) => (
@@ -28,9 +33,29 @@ export default function Footer() {
             </div>
           ))}
         </nav>
-        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          &copy; 2023 Manoir Rockland Manor. All rights reserved.
-        </p>
+        <div className="mt-10 flex flex-row items-center justify-between">
+          <p className=" text-center text-xs leading-5 text-gray-500">
+            &copy; 2023 Manoir Rockland Manor. All rights reserved.
+          </p>
+
+          <div className="text-sm text-gray-600">
+            <Link
+              href="/"
+              locale="fr"
+              className={cn(locale === "fr" ? "font-medium" : "")}
+            >
+              Français
+            </Link>{" "}
+            |{" "}
+            <Link
+              href="/"
+              locale="en"
+              className={cn(locale === "en" ? "font-medium" : "")}
+            >
+              English
+            </Link>
+          </div>
+        </div>
       </div>
     </footer>
   );
